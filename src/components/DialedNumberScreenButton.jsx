4a4e886f1c2bda6eb09652f131fa21743d2e63b4
Mirza Unas <unasmirza316@gmail.com>
2024-10-31 14:04:12 +0500

    Adding cancel call functionality

--- src/components/DialedNumberScreenButton.jsx
+++ src/components/DialedNumberScreenButton.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const DialedNumberScreenButton = ({ icon, label, disabled, cancel }) => {
   return (
@@ -8,7 +9,14 @@ const DialedNumberScreenButton = ({ icon, label, disabled, cancel }) => {
           cancel ? "bg-red-600" : "bg-gray-700"
         } ${disabled ? "opacity-50 cursor-not-allowed" : "cursor-pointer"} `}
       >
-        <span className="text-xl">{icon}</span>
+        {cancel ? (
+          <Link to={"/call-log"} className=" text-xl">
+            {" "}
+            {icon}{" "}
+          </Link>
+        ) : (
+          <span className="text-xl">{icon}</span>
+        )}
       </div>
       <p className="mt-2 text-sm">{label}</p>
     </div>
