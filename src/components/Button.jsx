83a71994ab3e4233e70b932be3ef62b5a38d7019
Mirza Unas <unasmirza316@gmail.com>
2024-10-31 12:47:04 +0500

    Adding vercel file

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -7,7 +7,7 @@ const Button = ({ number, letters }) => {
   return (
     <button
       onClick={() => dispatch(addDialNumber(number))}
-      className=" flex items-center justify-center flex-col bg-zinc-800 rounded-full text-gray-100 text-2xl w-16 h-16 cursor-pointer"
+      className=" flex items-center justify-center flex-col bg-zinc-800 rounded-full text-gray-100 text-2xl w-16 h-16 cursor-pointer pt-2"
     >
       <span className="  ">{number}</span>
       {letters && <p className="text-xs text-gray-400">{letters}</p>}
